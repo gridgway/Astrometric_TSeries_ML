38e99d098b5fc1da3e71c9860b3ccf4b9a17c7bd
gridgway <gregridgway@gmail.com>
2022-05-27 15:35:27 -0400

    Added comments, got rid of blank space

--- models/sets.py
+++ models/sets.py
@@ -27,56 +27,66 @@ class SetTransformerLSTM(nn.Module):
     def forward(self, x):
 
         batch_size = x.shape[0]
-        
+
         # Combine batch and number of stars into a single 'batch' dimension
         x = rearrange(x, 'b s t xy -> (b s) t xy')
-        
+
         # Encode all through LSTM
-        x, (h, c) = self.phi(x) 
-        
+        x, (h, c) = self.phi(x)
+
         # Extract two 'hidden' vectors corresponding to two directions in LSTM
         h = rearrange(h, '(d l) b h -> b d l h', d=2)[:, :, -1, :]
         h = rearrange(h, 'b d h -> b (d h)')
-        
+
         # Uncombine batch and star dimensions
         x = rearrange(h, '(b s) h -> b s h', b=batch_size)
 
         return self.dec(self.enc(x))[:, 0, :]
+
 
 class DeepSetLSTM(nn.Module):
     def __init__(self, hidden_dim=64, output_dim=1):
         """ A deep set model based on an LSTM encoder
         """
         super(DeepSetLSTM, self).__init__()
-                    
-        # Bi-dirctional LSTM encoder
-        self.phi = nn.LSTM(input_size=2, hidden_size=hidden_dim, num_layers=8, batch_first=True, bidirectional=True)
-        self.phi_mlp = build_mlp(input_dim=2 * hidden_dim, hidden_dim=4 * hidden_dim, output_dim=2 * hidden_dim, layers=4)
-        
+
+        # Bi-directional LSTM encoder
+        self.phi = nn.LSTM(
+            input_size=2, hidden_size=hidden_dim,
+            num_layers=8, batch_first=True, bidirectional=True
+        )
+        self.phi_mlp = build_mlp(
+            input_dim=2 * hidden_dim, hidden_dim=4 * hidden_dim,
+            output_dim=2 * hidden_dim, layers=4
+        )
+
         # MLP post-aggregation network
-        self.rho = build_mlp(input_dim=2 * hidden_dim, hidden_dim=2 * hidden_dim, output_dim=output_dim, layers=4)
+        self.rho = build_mlp(
+            input_dim=2 * hidden_dim, hidden_dim=2 * hidden_dim,
+            output_dim=output_dim, layers=4
+        )
 
     def forward(self, x):
-        
+
         batch_size = x.shape[0]
-        
+
         # Combine batch and number of stars into a single 'batch' dimension
         x = rearrange(x, 'b s t xy -> (b s) t xy')
-        
+
         # Encode all through LSTM
-        x, (h, c) = self.phi(x) 
-        
+        x, (h, c) = self.phi(x)
+
         # Extract two 'hidden' vectors corresponding to two directions in LSTM
         h = rearrange(h, '(d l) b h -> b d l h', d=2)[:, :, -1, :]
         h = rearrange(h, 'b d h -> b (d h)')
-        
+
         h = self.phi_mlp(h)
 
         # Uncombine batch and star dimensions, then do mean-aggregation along star axis
         # (Enforce permutation invariance)
         h = rearrange(h, '(b s) h -> b s h', b=batch_size).mean(-2)
-        
+
         # Pass through post-aggregation network
         x = self.rho(h)
-        
+
         return x
--- models/spatio_temporal.py
+++ models/spatio_temporal.py
@@ -27,19 +27,28 @@ class SpatioTemporalLSTM(nn.Module):
     def forward(self, x):
 
         batch_size = x.shape[0]
-        
+
         # Combine batch and number of stars into a single 'batch' dimension
+        # b = batch, s = star, t = time, xy = angular (x,y) coordinate
         x = rearrange(x, 'b s t xy -> (b t) s xy')
+        # !!! Is it wrong to make interleave b and t?
 
         x = self.enc(x)
-        x = self.dec(x).squeeze()
+        x = self.dec(x).squeeze()  # s dimension eliminated
+        # At this point, you have summaries of time sieres 1 at time 1 ...
+        # time series N at time L
 
         x = rearrange(x, '(b t) h -> b t h', b=batch_size)
 
-        x, (h, c) = self.phi(x) 
-                
+        x, (h, c) = self.phi(x)  # apply LSTM: t dimension eliminated
+        # At this point you have summaries for time series 1 ... time series N
+
         # Extract two 'hidden' vectors corresponding to two directions in LSTM
+        # d = direction, l = layer, b = batch, h = hidden
+        # Keep only last layer, but both directions
         h = rearrange(h, '(d l) b h -> b d l h', d=2)[:, :, -1, :]
         h = rearrange(h, 'b d h -> b (d h)')
-        
+
+        # Your summary for each time series is (direc) x (dim-h) dimensional = 2 x dim_hidden
+
         return h
